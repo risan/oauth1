d6762879b86f6fd28e2dae8c5cc5bcd04099c483
risan <risanbagja@gmail.com>
2015-12-23 11:57:23 +0100

    Extract each OAuth flow into separtated trait implementation.

--- src/Contracts/OAuth1Flows/TokenCredentialsFlowInterface.php
+++ src/Contracts/OAuth1Flows/TokenCredentialsFlowInterface.php
@@ -4,9 +4,26 @@ namespace OAuth1Client\Contracts\OAuth1Flows;
 
 interface TokenCredentialsFlowInterface {
     /**
-     * Access token credentials url.
+     * Token credentials.
+     *
+     * @param  OAuth1Client\Contracts\Credentials\TemporaryCredentialsInterface $temporaryCredentials
+     * @param  string   $temporaryIdentifier
+     * @param  string   $verifier
+     * @return OAuth1Client\Contracts\Credentials\TokenCredentialsInterface                                              [description]
+     */
+    public function tokenCredentials(TemporaryCredentialsInterface $temporaryCredentials, $temporaryIdentifier, $verifier);
+
+    /**
+     * Token credentials url.
      *
      * @return string
      */
     public function tokenCredentialsUrl();
+
+    /**
+     * Token credentials header.
+     *
+     * @return array
+     */
+    public function tokenCredentialsHeaders();
 }
