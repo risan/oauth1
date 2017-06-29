f425b5795eda10716f34a78d5a609b902bedb540
risan <risanbagja@gmail.com>
2017-06-29 13:10:00 +0200

    :sparkles: Add Credentials abstract class.

---
+++ src/Credentials/Credentials.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Risan\OAuth1\Credentials;
+
+abstract class Credentials implements CredentialsInterface
+{
+    /**
+     * The credentials identifier.
+     *
+     * @var string
+     */
+    protected $identifier;
+
+    /**
+     * The credentials shared-secret.
+     *
+     * @var string
+     */
+    protected $secret;
+
+    /**
+     * Creaate the new Crendentials class instance.
+     *
+     * @param string $identifier
+     * @param string $secret
+     */
+    public function __construct($identifier, $secret)
+    {
+        $this->identifier = $identifier;
+        $this->secret = $secret;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+}
