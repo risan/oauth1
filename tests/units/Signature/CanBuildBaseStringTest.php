30663d8ac1bb399b8daa50887906470885b2390d
risan <risanbagja@gmail.com>
2017-07-25 19:12:00 +0200

    :white_check_mark: Update the CanBuildBaseString test trait.

--- tests/units/Signature/CanBuildBaseStringTest.php
+++ tests/units/Signature/CanBuildBaseStringTest.php
@@ -6,23 +6,24 @@ use Risan\OAuth1\Signature\BaseStringBuilderInterface;
 
 class CanBuildBaseStringTest extends TestCase
 {
-    private $canBuildBaseString;
+    private $canBuildBaseStringStub;
 
     function setUp()
     {
-        $this->canBuildBaseString = $this->getMockForTrait(CanBuildBaseString::class);
+        $this->canBuildBaseStringStub = $this->getMockForTrait(CanBuildBaseString::class);
     }
 
     /** @test */
     function can_build_base_string_trait_can_get_base_string_builder_interface_instance()
     {
-        $this->assertInstanceOf(BaseStringBuilderInterface::class, $this->canBuildBaseString->getBaseStringBuilder());
+        $this->assertInstanceOf(BaseStringBuilderInterface::class, $this->canBuildBaseStringStub->getBaseStringBuilder());
     }
 
     /** @test */
     function can_build_base_string_trait_can_build_base_string()
     {
-        $baseString = $this->canBuildBaseString->buildBaseString('http://example.com/path', ['foo' => 'bar'], 'POST');
-        $this->assertEquals('POST&http%3A%2F%2Fexample.com%2Fpath&foo%3Dbar', $baseString);
+        $baseString = $this->canBuildBaseStringStub->buildBaseString('http://example.com/path', ['foo' => 'bar'], 'POST');
+
+        $this->assertEquals('POST&http%3A%2F%2Fexample.com%2Fpath&foo%3Dbar', $canBuildBaseStringStub);
     }
 }
