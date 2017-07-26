597b5b38237a71835320c4032844ed50c1a23d25
risan <risanbagja@gmail.com>
2017-07-26 19:10:00 +0200

    :sparkles: Add new interface for signer.

---
+++ src/Signature/SignerInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Risan\OAuth1\Signature;
+
+interface SignerInterface
+{
+    /**
+     * Get signer method name.
+     *
+     * @return string
+     */
+    public function method();
+
+    /**
+     * Create a signature for given request parameters.
+     *
+     * @param  string $uri
+     * @param  array  $parameters
+     * @param  string $httpMethod
+     * @return string
+     */
+    public function sign($uri, array $parameters = [], $httpMethod = 'POST');
+}
