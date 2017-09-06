4275e1f201eba6604d33a65da207735ce2b66416
risan <risanbagja@gmail.com>
2017-09-06 16:20:00 +0200

    :sparkles: Add new BuilderInterface.

---
+++ src/Request/BuilderInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Risan\OAuth1\Request;
+
+interface BuilderInterface
+{
+    /**
+     * Get the ConfigInterface instance.
+     *
+     * @return \Risan\OAuth1\ConfigInterface
+     */
+    public function getConfig();
+
+    /**
+     * Get the SignerInterface instance.
+     *
+     * @return \Risan\OAuth1\Signature\SignerInterface
+     */
+    public function getSigner();
+
+    /**
+     * Get the NonceGeneratorInterface instance.
+     *
+     * @return \Risan\OAuth1\Request\NonceGeneratorInterface
+     */
+    public function nonceGenerator();
+
+    /**
+     * Get current timestamp in seconds since Unix Epoch.
+     *
+     * @return string
+     */
+    public function getCurrentTimestamp();
+
+    /**
+     * Get url for obtaining temporary credentials.
+     *
+     * @return string
+     */
+    public function getTemporaryCredentialsUrl();
+
+    /**
+     * Get authorization header for obtaining temporary credentials.
+     *
+     * @return string
+     */
+    public function getTemporaryCredentialsAuthorizationHeader();
+}
