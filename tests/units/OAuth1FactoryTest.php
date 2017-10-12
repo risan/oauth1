f1f6d4bea321135cccd78ceadc96156acb550874
risan <risanbagja@gmail.com>
2017-10-13 01:03:53 +0200

    :white_check_mark: Add OAuth1Factory test.

---
+++ tests/units/OAuth1FactoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Risan\OAuth1\OAuth1;
+use PHPUnit\Framework\TestCase;
+use Risan\OAuth1\OAuth1Factory;
+
+class OAuth1FactoryTest extends TestCase
+{
+    private $configParams;
+
+    function setUp()
+    {
+        $this->configParams = [
+            'client_credentials_identifier' => 'client_id',
+            'client_credentials_secret' => 'client_secret',
+            'temporary_credentials_url' => 'http://example.com/temporary_credentials_url',
+            'callback_uri' => 'http://example.com/callback_uri',
+        ];
+    }
+
+    /** @test */
+    function oauth1_factory_can_create_oauth1_instance()
+    {
+        $this->assertInstanceOf(OAuth1::class, OAuth1Factory::create($this->configParams));
+    }
+}
