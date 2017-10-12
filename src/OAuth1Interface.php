e024ba186ec4f1552c8222f9b1cd783ecc6e58b5
risan <risanbagja@gmail.com>
2017-10-12 22:37:00 +0200

    :sparkles: Add OAuth1Interface.

---
+++ src/OAuth1Interface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Risan\OAuth1;
+
+interface OAuth1Interface
+{
+    /**
+     * Get the RequestConfigInterface instance.
+     *
+     * @return \Risan\OAuth1\ConfigInterface
+     */
+    public function getRequestConfig();
+
+    /**
+     * Get the HTTP client interface instance.
+     *
+     * @return \Risan\OAuth1\HttpClientInterface
+     */
+    public function getHttpClient();
+
+    /**
+     * Obtain the temporary credentials.
+     *
+     * @return \Risan\OAuth1\Credentials\TemporaryCredentials
+     */
+    public function getTemporaryCredentials();
+}
