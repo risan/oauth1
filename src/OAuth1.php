cb6405a0507d2f2d5f4db5e1ae49f7e4c2f48f61
risan <risanbagja@gmail.com>
2017-10-12 22:49:46 +0200

    :sparkles: Add OAuth1 implementation.

---
+++ src/OAuth1.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Risan\OAuth1;
+
+use Risan\OAuth1\Request\RequestConfigInterface;
+
+class OAuth1 implements OAuth1Interface
+{
+    /**
+     * The RequestConfigInterface instance.
+     *
+     * @var \Risan\OAuth1\Request\RequestConfig
+     */
+    protected $requestConfig;
+
+    /**
+     * The HttpClientInterface instance.
+     *
+     * @var \Risan\OAuth1\HttpClientInterface
+     */
+    protected $httpClient;
+
+    /**
+     * Create a new OAuth1 instance.
+     *
+     * @param \Risan\OAuth1\Request\RequestConfigInterface $requestConfig
+     * @param \Risan\OAuth1\HttpClientInterface $httpClient
+     */
+    public function __construct(RequestConfigInterface $requestConfig, HttpClientInterface $httpClient)
+    {
+        $this->requestConfig = $requestConfig;
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getRequestConfig()
+    {
+        return $this->requestConfig;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getHttpClient()
+    {
+        return $this->httpClient;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTemporaryCredentials()
+    {
+        return $this->httpClient->post($this->requestConfig->getTemporaryCredentialsUrl(), [
+            'headers' => [
+                'Authorization' => $this->requestConfig->getTemporaryCredentialsAuthorizationHeader(),
+            ],
+        ]);
+    }
+}
