38b7bf5bc93ac44125aea037b7766683e4278124
risan <risanbagja@gmail.com>
2017-10-12 23:08:37 +0200

    :white_check_mark: Add test for OAuth1.

---
+++ tests/units/OAuth1Test.php
@@ -0,0 +1,68 @@
+<?php
+
+use Risan\OAuth1\OAuth1;
+use PHPUnit\Framework\TestCase;
+use Risan\OAuth1\OAuth1Interface;
+use Risan\OAuth1\HttpClientInterface;
+use Psr\Http\Message\ResponseInterface;
+use Risan\OAuth1\Credentials\ClientCredentials;
+use Risan\OAuth1\Request\RequestConfigInterface;
+
+class OAuth1Test extends TestCase
+{
+    private $requestConfigStub;
+    private $httpClientStub;
+    private $oauth1;
+
+    function setUp()
+    {
+        $this->requestConfigStub = $this->createMock(RequestConfigInterface::class);
+        $this->httpClientStub = $this->createMock(HttpClientInterface::class);
+        $this->oauth1 = new OAuth1($this->requestConfigStub, $this->httpClientStub);
+    }
+
+    /** @test */
+    function oauth1_is_an_instance_of_oauth1_interface()
+    {
+        $this->assertInstanceOf(OAuth1Interface::class, $this->oauth1);
+    }
+
+    /** @test */
+    function oauth1_can_get_request_config()
+    {
+        $this->assertInstanceOf(RequestConfigInterface::class, $this->oauth1->getRequestConfig());
+    }
+
+    /** @test */
+    function oauth1_can_get_http_client()
+    {
+        $this->assertInstanceOf(HttpClientInterface::class, $this->oauth1->getHttpClient());
+    }
+
+    /** @test */
+    function oauth1_can_obtain_temporary_credentials()
+    {
+        $this->requestConfigStub
+            ->expects($this->once())
+            ->method('getTemporaryCredentialsUrl')
+            ->willReturn('http://example.com/temporary_credentials_url');
+
+        $this->requestConfigStub
+            ->expects($this->once())
+            ->method('getTemporaryCredentialsAuthorizationHeader')
+            ->willReturn('Authorization Header');
+
+        $responseStub = $this->createMock(ResponseInterface::class);
+
+        $this->httpClientStub
+            ->expects($this->once())
+            ->method('post')
+            ->with(
+                'http://example.com/temporary_credentials_url',
+                ['headers' => ['Authorization' => 'Authorization Header']]
+            )
+            ->willReturn($responseStub);
+
+        $this->assertInstanceOf(ClientCredentials::class, $this->oauth1->getTemporaryCredentials());
+    }
+}
