e6a637c74bf323b140ed031c3a1307e67d6d01a5
risan <risanbagja@gmail.com>
2017-10-14 23:05:29 +0200

    :sparkles: Add ConfigInterface.

---
+++ src/Config/ConfigInterface.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Risan\OAuth1\Config;
+
+interface ConfigInterface
+{
+    /**
+     * Get the ClientCredentials instance.
+     *
+     * @return \Risan\OAuth1\Credentials\ClientCredentials
+     */
+    public function getClientCredentials();
+
+    /**
+     * Get the client credentials identifer.
+     *
+     * @return string
+     */
+    public function getClientCredentialsIdentifier();
+
+    /**
+     * Get the client credentials secret.
+     *
+     * @return string
+     */
+    public function getClientCredentialsSecret();
+
+    /**
+     * Get the UriConfigInterface instance.
+     *
+     * @return \Risan\OAuth1\Config\UriConfigInterface
+     */
+    public function getUri();
+
+    /**
+     * Get the URI for obtaining temporary credentials. Also known as request
+     * token URI.
+     *
+     * @return string
+     */
+    public function getTemporaryCredentialsUri();
+
+    /**
+     * Get the URI for asking user to authorize the request.
+     *
+     * @return string
+     */
+    public function getAuthorizationUri();
+
+    /**
+     * Get the URI for obtaining token credentials. Also known as access token
+     * URI.
+     *
+     * @return string
+     */
+    public function getTokenCredentialsUri();
+
+    /**
+     * Check if callback URI is set.
+     *
+     * @return boolean
+     */
+    public function hasCallbackUri();
+
+    /**
+     * Get the callback URI.
+     *
+     * @return string|null
+     */
+    public function getCallbackUri();
+
+    /**
+     * Create an instance of ConfigureInterface from an array.
+     *
+     * @return \Risan\OAuth1\Config\ConfigInterface
+     * @throws \InvalidArgumentException
+     */
+    public static function createFromArray(array $config);
+}
