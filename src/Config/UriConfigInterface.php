3af15655de74c313c984503e5e3fb65a270485a8
risan <risanbagja@gmail.com>
2017-10-14 22:15:04 +0200

    :sparkles: Add UriConfig class and interface.

---
+++ src/Config/UriConfigInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Risan\OAuth1\Config;
+
+interface UriConfigInterface
+{
+    /**
+     * Get the UriParserInterface implementation.
+     *
+     * @return \Risan\OAuth1\Request\UriParserInterface
+     */
+    public function parser();
+
+    /**
+     * Get the base URI.
+     *
+     * @return \Psr\Http\Message\UriInterface|null
+     */
+    public function base();
+
+    /**
+     * Check if base URI is set.
+     *
+     * @return boolean
+     */
+    public function hasBase();
+
+    /**
+     * Get the URI for obtaining temporary credentials. Also known as request
+     * token URI.
+     *
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function temporaryCredentials();
+
+    /**
+     * Get the URI for asking user to authorize the request.
+     *
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function authorization();
+
+    /**
+     * Get the URI for obtaining token credentials. Also known as access token
+     * URI.
+     *
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function tokenCredentials();
+
+    /**
+     * Get the callback URI.
+     *
+     * @return \Psr\Http\Message\UriInterface|null
+     */
+    public function callback();
+
+    /**
+     * Check if callback URI is set.
+     *
+     * @return boolean
+     */
+    public function hasCallback();
+}
