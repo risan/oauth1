c4c267f4f4a1bbf36bb0b01a1ac2a22743e1f81e
risan <risanbagja@gmail.com>
2017-10-14 18:25:13 +0200

    :sparkles: Add UriConfig interface.

---
+++ src/Config/UriConfigInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Risan\OAuth1\Config;
+
+interface UriConfigInterface
+{
+    /**
+     * Get the URI for obtaining temporary credentials. Also known as request
+     * token URI.
+     *
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function temporaryCredentials();
+
+    /**
+     * Get the URI for asking user to authorize the request.
+     *
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function authorization();
+
+    /**
+     * Get the URI for obtaining token credentials. Also known as access token
+     * URI.
+     *
+     * @return \Psr\Http\Message\UriInterface
+     */
+    public function tokenCredentials();
+
+    /**
+     * Get the callback URI.
+     *
+     * @return \Psr\Http\Message\UriInterface|null
+     */
+    public function callback();
+
+    /**
+     * Check if callback URI is set.
+     *
+     * @return boolean
+     */
+    public function hasCallback();
+}
