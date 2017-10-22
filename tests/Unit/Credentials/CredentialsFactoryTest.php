b385211526a1848e2d81e2729afb8b3e5ce413d0
risan <risanbagja@gmail.com>
2017-10-22 10:19:40 +0200

    :white_check_mark: Update credentials factory test.

--- tests/Unit/Credentials/CredentialsFactoryTest.php
+++ tests/Unit/Credentials/CredentialsFactoryTest.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ResponseInterface;
 use Risan\OAuth1\Credentials\CredentialsFactory;
 use Risan\OAuth1\Credentials\CredentialsException;
 use Risan\OAuth1\Credentials\TemporaryCredentials;
+use Risan\OAuth1\Credentials\CredentialsFactoryInterface;
 
 class CredentialsFactoryTest extends TestCase
 {
@@ -23,42 +24,64 @@ class CredentialsFactoryTest extends TestCase
     }
 
     /** @test */
-    function it_throws_credentials_exception_when_creating_temporary_credentials_and_oauth_token_parameter_is_missing()
+    function it_implements_credentials_factory_interface()
+    {
+        $this->assertInstanceOf(CredentialsFactoryInterface::class, $this->credentialsFactory);
+    }
+
+    /** @test */
+    function it_can_get_parameters_from_response()
+    {
+        $this->setupResponseStub('foo=bar');
+
+        $this->assertSame(
+            ['foo' => 'bar'],
+            $this->credentialsFactory->getParametersFromResponse($this->responseStub)
+        );
+    }
+
+    /** @test */
+    function it_can_get_missing_parameter_key()
+    {
+        $this->assertEquals(
+            'baz',
+            $this->credentialsFactory->getMissingParameterKey(['foo' => 'bar'], ['foo', 'baz'])
+        );
+
+        $this->assertNull(
+            $this->credentialsFactory->getMissingParameterKey(['foo' => 'bar'], ['foo'])
+        );
+    }
+
+    /** @test */
+    function it_throws_exception_when_creating_temporary_credentials_and_oauth_token_is_missing()
     {
         $this->setupResponseStub('oauth_token_secret=token_secret&oauth_callback_confirmed=true');
-
         $this->expectException(CredentialsException::class);
-
         $this->credentialsFactory->createTemporaryCredentialsFromResponse($this->responseStub);
     }
 
     /** @test */
-    function it_throws_credentials_exception_when_creating_temporary_credentials_and_oauth_token_secret_parameter_is_missing()
+    function it_throws_exception_when_creating_temporary_credentials_and_oauth_token_secret_is_missing()
     {
         $this->setupResponseStub('oauth_token=token_id&oauth_callback_confirmed=true');
-
         $this->expectException(CredentialsException::class);
-
         $this->credentialsFactory->createTemporaryCredentialsFromResponse($this->responseStub);
     }
 
     /** @test */
-    function it_throws_credentials_exception_when_creating_temporary_credentials_and_oauth_callback_confirmed_parameter_is_missing()
+    function it_throws_exception_when_creating_temporary_credentials_and_oauth_callback_confirmed_is_missing()
     {
         $this->setupResponseStub('oauth_token=token_id&oauth_token_secret=token_secret');
-
         $this->expectException(CredentialsException::class);
-
         $this->credentialsFactory->createTemporaryCredentialsFromResponse($this->responseStub);
     }
 
     /** @test */
-    function it_throws_credentials_exception_when_creating_temporary_credentials_and_oauth_callback_confirmed_is_not_true()
+    function it_throws_exception_when_creating_temporary_credentials_and_oauth_callback_confirmed_is_not_true()
     {
         $this->setupResponseStub('oauth_token=token_id&oauth_token_secret=token_secret&oauth_callback_confirmed=false');
-
         $this->expectException(CredentialsException::class);
-
         $this->credentialsFactory->createTemporaryCredentialsFromResponse($this->responseStub);
     }
 
@@ -66,7 +89,6 @@ class CredentialsFactoryTest extends TestCase
     function it_can_create_temporary_credentials_from_response()
     {
         $this->setupResponseStub('oauth_token=token_id&oauth_token_secret=token_secret&oauth_callback_confirmed=true');
-
         $temporaryCredentials = $this->credentialsFactory->createTemporaryCredentialsFromResponse($this->responseStub);
 
         $this->assertInstanceOf(TemporaryCredentials::class, $temporaryCredentials);
