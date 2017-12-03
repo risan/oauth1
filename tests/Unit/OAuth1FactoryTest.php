0fb37df8e53c796bf8d8c6c460b4136844c79ba4
risan <risanbagja@gmail.com>
2017-12-03 18:31:34 +0100

    :white_check_mark: Add test for custom signer.

--- tests/Unit/OAuth1FactoryTest.php
+++ tests/Unit/OAuth1FactoryTest.php
@@ -7,10 +7,12 @@ use PHPUnit\Framework\TestCase;
 use Risan\OAuth1\OAuth1Factory;
 use Risan\OAuth1\OAuth1Interface;
 use Risan\OAuth1\Signature\HmacSha1Signer;
+use Risan\OAuth1\Signature\PlainTextSigner;
 
 class OAuth1FactoryTest extends TestCase
 {
     private $config;
+    private $plainTextSignerStub;
 
     function setUp()
     {
@@ -23,6 +25,8 @@ class OAuth1FactoryTest extends TestCase
             'token_credentials_uri' => '/access_token',
             'callback_uri' => 'http://johndoe.net',
         ];
+
+        $this->plainTextSignerStub = $this->createMock(PlainTextSigner::class);
     }
 
     /** @test */
@@ -37,6 +41,21 @@ class OAuth1FactoryTest extends TestCase
             $oauth1->getRequestFactory()->getAuthorizationHeader()->getProtocolParameter()->getSigner()
         );
     }
+
+    
+    /** @test */
+    function it_accepts_custom_signer_parameter()
+    {
+        $oauth1 = OAuth1Factory::create($this->config, $this->plainTextSignerStub);
+
+        $this->assertInstanceOf(OAuth1Interface::class, $oauth1);
+
+        $this->assertInstanceOf(
+            PlainTextSigner::class,
+            $oauth1->getRequestFactory()->getAuthorizationHeader()->getProtocolParameter()->getSigner()
+        );
+    }
+
 
     /** @test */
     function it_throws_exception_if_signer_not_implements_signer_interface()
